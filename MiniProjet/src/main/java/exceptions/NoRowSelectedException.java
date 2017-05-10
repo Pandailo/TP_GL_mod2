84efc7c39aa8c1de267986edd1e333732175a00a
Pandailo <veron.yann@gmail.com>
2017-05-10 11:15:17 +0200

    Debut des TU

--- MiniProjet/src/main/java/exceptions/NoRowSelectedException.java
+++ MiniProjet/src/main/java/exceptions/NoRowSelectedException.java
@@ -3,7 +3,7 @@ package main.java.exceptions;
 public class NoRowSelectedException extends Exception {
 
 	/**
-	 * 
+	 * Exception levée si la requête ne retourne aucune colonne 
 	 */
 	private static final long serialVersionUID = -6916954820331775017L;
 
---
+++ MiniProjet/src/test/java/service/impl/MusiqueServiceImplTest.java
@@ -0,0 +1,27 @@
+package test.java.service.impl;
+
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+
+
+public class MusiqueServiceImplTest {
+	@Rule
+	ExpectedException exception=ExpectedException.none();
+	@BeforeClass
+	public void init()
+	{
+		
+	}
+	
+	@Test
+	public void testEcrireFichier()
+	{
+		
+	}
+	@Test
+	public void testLireFichier()
+	{
+		
+	}
+
+}
