6f3f7c51d2fdac67dfc7ba1252eb3db9f72865e6
Pandailo <veron.yann@gmail.com>
2017-05-12 09:13:29 +0200

    Ajout machins TRUC

---
+++ MiniProjet/init_sql.sql
@@ -0,0 +1,16 @@
+DROP TABLE PGL_Musique;
+CREATE TABLE PGL_Musique(id INTEGER,titre VARCHAR2(25),duree INTEGER,artiste VARCHAR2(50));
+CREATE OR REPLACE TRIGGER trigger_PGL_id BEFORE INSERT ON PGL_Musique FOR EACH ROW
+DECLARE
+	idm INTEGER;
+BEGIN
+	SELECT max(id) INTO idm FROM PGL_Musique;
+	IF idm IS NOT NULL
+	THEN 
+		idm:=idm+1; 
+		:new.id := idm;
+	ELSE
+		:new.id :=0;
+	END IF;
+END;
+/
